c60d6f06d6e405caeea084bf2273bc5d5f5d68d6
albianto97 <albyantox@gmail.com>
2026-01-07 20:42:25 +0100

    css ranking.component.css

--- pingpong-frontend/src/app/features/ranking/ranking.component.ts
+++ pingpong-frontend/src/app/features/ranking/ranking.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../core/services/user.service';
 import { AuthService } from '../../core/services/auth.service';
-import {RouterLink} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './ranking.component.html',
   styleUrls: ['./ranking.component.css']
 })
@@ -16,24 +16,31 @@ export class RankingComponent implements OnInit {
   ranking: any[] = [];
   currentUserId?: string;
   loading = true;
+  meId!: string;
+
 
   constructor(
     private userService: UserService,
-    private authService: AuthService
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    const user = this.authService.getCurrentUser();
-    this.currentUserId = user?._id;
+    this.authService.user$.subscribe(me => {
+      if (!me) return;
 
-    this.userService.getRanking().subscribe({
-      next: data => {
-        this.ranking = data;
-        this.loading = false;
-      },
-      error: () => {
-        this.loading = false;
-      }
+      this.meId = me._id;
+
+      this.userService.getRanking().subscribe(ranking => {
+        this.ranking = ranking;
+      });
     });
+  }
+
+  isMe(player: any) {
+    return player.id === this.meId;
+  }
+  goToProfile(userId: string) {
+    this.router.navigate(['/profile', userId]);
   }
 }
