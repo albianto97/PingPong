669bdf7c491ebc1c0c674552b950e12c20b7d0a8
albianto97 <36625340+albianto97@users.noreply.github.com>
2026-01-08 16:38:27 +0100

    Update tournament-bracket.component.ts

--- pingpong-frontend/src/app/features/tournaments/tournament-bracket/tournament-bracket.component.ts
+++ pingpong-frontend/src/app/features/tournaments/tournament-bracket/tournament-bracket.component.ts
@@ -1,13 +1,26 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TournamentMatchFormComponent } from '../tournament-match-form/tournament-match-form.component';
 
 @Component({
   selector: 'app-tournament-bracket',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TournamentMatchFormComponent],
   templateUrl: './tournament-bracket.component.html',
   styleUrls: ['./tournament-bracket.component.css']
 })
 export class TournamentBracketComponent {
+
   @Input() rounds: any[] = [];
+  selectedMatch: any = null;
+
+  openMatch(match: any) {
+    if (match.winner) return;
+    this.selectedMatch = match;
+  }
+
+  onMatchSaved() {
+    this.selectedMatch = null;
+    location.reload(); // semplice per ora
+  }
 }
