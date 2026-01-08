3f2cf602ad8aa52000d1b78d6114bda4c7216dab
albianto97 <albyantox@gmail.com>
2026-01-08 22:05:30 +0100

    testare i tornei

---
+++ pingpong-frontend/src/app/features/tournaments/tournament-create/tournament-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-create',
+  imports: [],
+  templateUrl: './tournament-create.component.html',
+  styleUrl: './tournament-create.component.css'
+})
+export class TournamentCreateComponent {
+
+}
